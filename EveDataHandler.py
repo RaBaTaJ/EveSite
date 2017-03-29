d22eae0008dee488d54cd1d4a1a0271778b4748a
Renaud Rabusseau <Renaud Rabusseau>
2017-03-29 20:58:14 +0200

    TEST REFFERALS

--- EveDataHandler.py
+++ EveDataHandler.py
@@ -128,6 +128,7 @@ def UpdateDB():
                 Cursor.execute('INSERT INTO WalletJournal_charactertotalinvestment VALUES ({0},"{1}",{2},{3},{4},{5})'.format(NewID, row[4], row[1], 0, 0, 0))
                 Comment = row[3]
                 Comment = Comment[6:]
+                print(Comment)
 
 #TEST CODE FOR REFFERALS
                 if Comment in InvestorList:
